98841dc8bcebd62facf34f3abe5851567977edd9
jimmyjames88 <james.allen@fyidoctors.com>
2021-04-25 18:55:18 -0600

    updated dependencies, rebuilt scss

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,27 +1,42 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var connect = require('gulp-connect-php7');
+'use strict'
+const { src, dest } = require('gulp');
+const sass = require('gulp-sass');
 
-gulp.task('connect', function(){
-	connect.server({
-		base: 'dist',
-		port: 3000
-	})
-})
+function compileSass(done) {
+	src('dev/sass/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(dest('dev/css'));
+		done();
+}
 
-gulp.task('watch', function(){
-	gulp.watch('dev/**/*',['build']);
-});
+exports.compileSass = compileSass;
 
-gulp.task('build', function() {
 
-	gulp.src(['dev/**/*', '!dev/sass/**/*', '!dev/sass/'])
-		.pipe(gulp.dest('dist'));
 
-    gulp.src('dev/sass/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('dist/css'))
-});
+// var gulp = require('gulp');
+// var sass = require('gulp-sass');
+// var connect = require('gulp-connect-php7');
 
-//Watch task
-gulp.task('default',['build', 'connect', 'watch']);
+// gulp.task('connect', function(){
+// 	connect.server({
+// 		base: 'dist',
+// 		port: 3000
+// 	})
+// })
+
+// gulp.task('watch', function(){
+// 	gulp.watch('dev/**/*',['build']);
+// });
+
+// gulp.task('build', function() {
+
+// 	gulp.src(['dev/**/*', '!dev/sass/**/*', '!dev/sass/'])
+// 		.pipe(gulp.dest('dist'));
+
+//     gulp.src('dev/sass/**/*.scss')
+//         .pipe(sass().on('error', sass.logError))
+//         .pipe(gulp.dest('dist/css'))
+// });
+
+// //Watch task
+// gulp.task('default',['build', 'connect', 'watch']);
