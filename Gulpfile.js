2e2a26d451fbf76668b0f433f71b957d9fea8dd3
jimmyjames88 <james.allen@fyidoctors.com>
2021-04-26 08:24:02 -0600

    restructure

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,15 +1,57 @@
 'use strict'
-const { src, dest } = require('gulp');
+const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass');
+const phpConnect = require("gulp-connect-php");
+const clean = require('gulp-clean');
+const browsersync = require('browser-sync')
 
-function compileSass(done) {
+// Watch files
+function watchFiles() {
+    watch('./dev/scss/*', doSass);
+}
+
+//Php connect
+function connectsync() {
+    phpConnect.server({
+        // a standalone PHP server that browsersync connects to via proxy
+        port: 3000,
+        keepalive: true,
+        base: "dev"
+    }, function (){
+        browsersync({
+            proxy: '127.0.0.1:3000'
+        });
+    });
+}
+
+// BrowserSync Reload
+function browserSyncReload(done) {
+    browsersync.reload();
+    done();
+}
+
+function doSass(done) {
 	src('dev/sass/*.scss')
 		.pipe(sass().on('error', sass.logError))
 		.pipe(dest('dev/css'));
 		done();
 }
 
-exports.compileSass = compileSass;
+function cleanFiles(clean) {
+	return src('./assets/*', {
+		read: false
+	})
+	.pipe(clean());
+}
+
+function moveFiles() {
+	return src('dev/**/*')
+	.pipe(dest('./dist'))
+}
+
+exports.doSass = doSass;
+exports.connectsync = connectsync;
+exports.cleanFiles = cleanFiles;
 
 
 
---
+++ resume/index.php
@@ -0,0 +1,226 @@
+<div class="content paper">
+
+	<div class="">
+		<div class="row">
+			<div class="col-xs-5">
+				<h1>James Allen</h1>
+				<h2>Website Designer &amp; Developer</h2>
+			</div>
+			<div class="col-xs-7">
+				<p class="text-right">
+					<a href="/resume/James-Allen-Resume-April.pdf" target="_blank">
+						<strong>
+							<span class="pdficon"></span> View PDF Version
+						</strong>
+					</a><br />
+					Portfolio: <a href="http://james-allen.ca/">www.james-allen.ca</a><br />
+					Email: <a href="mailto:me@james-allen.ca">me@james-allen.ca</a><br />
+					Cell: <a href="tel:+1-403-702-2795">(403) 702-2795</a><br />
+				</p>
+			</div>
+		</div>
+		<div class="divider"></div>
+		<div class="row">
+			<div class="col-xs-12">
+				<p>
+					Life-long web developer with a keen sense of user experience seeking new and exciting opportunities.
+					Passionate about developing new industry-specific ideas into tangible online products and services. 
+				</p>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-12">
+				<h3>Technology</h3>
+				<div class="divider"></div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-12 col-sm-4">
+				<ul>
+					<li>PHP / Laravel</li>
+					<li>Javascript / VueJS</li>
+					<li>CSS3 / SASS / Tailwinds</li>
+					<li>HTML5/ Bootstrap 4 / Bulma</li>
+					<li>Python / Flask</li>
+					<li>NPM / Webpack / Gulp</li>
+				</ul>
+			</div>
+			<div class="col-xs-12 col-sm-4">
+				<ul>
+					<li>WebRTC / OpenTok</li>
+					<li>MariaDB / MySQL</li>
+					<li>Drupal 7 / Drupal Commerce</li>
+					<li>Mandrill / Paypal / GoogleMaps</li>
+					<li>AWS S3 & EC3 / Heroku</li>
+					<li>Google Cloud VM Instances</li>
+				</ul>
+			</div>
+			<div class="col-xs-12 col-sm-4">
+				<ul>
+					<li>Git / SVN</li>
+					<li>Photoshop</li>
+					<li>Linux</li>
+					<li>Windows</li>
+					<li>WAMP / LAMP / XAMPP</li>
+					<li>Agile / SCRUM</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-12">
+				<h3>Experience</h3>
+				<div class="divider"></div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-12">
+				<h4>FYidoctors</h4>
+				<h5>Software Developer, August 2019 - Present</h5>
+				<ul>
+					<li>Essential in creating and implementing a browser-based online virtual personal shopping experience using WebRTC.</li>
+					<li>Transitioned control of website and website applications from a 3rd party marketing group.</li>
+					<li>Developed several new custom modules for the content management system (Asgard CMS).</li>
+					<li>Improved existing modules, and remedied legacy bugs.</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-12">
+				<h4>InnoTech College</h4>
+				<h5>Fullstack Development Instructor, April 2018 - October 2019, September 2020 - April 2021</h5>
+				<ul>
+					<li>Develop teaching materials full-stack curriculum for the 6-month Fullstack Development Diploma.</li>
+					<li>Materials included: Presentations, demo packages, exercises, quizzes, and more.</li>
+					<li>Teach students how to develop websites using a predominantly open-source stack.</li>
+					<li>Prepare students for the industry and different scenarios they are likely to encounter.</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-12">
+				<h4>Coco Brooks</h4>
+				<h5>Contract Graphic/UI Design, March 2018 - Present</h5>
+				<ul>
+					<li>Design a variety of marketing materials including posters and digital menu boards.</li>
+					<li>Revamp existing dated website interfaces with exciting design and new technologies. </li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-12">
+				<h4>AthleteBook</h4>
+				<h5>Contract Web Developer, April 2017 – July 2017</h5>
+				<ul>
+					<li>Translated existing design PSD files in to usable visual components and Laravel Blade templates.</li>
+					<li>Managed new and existing workflow for an overseas development team utilizing Agile and JIRA.</li>
+					<li>Designed several visual components to be aesthetically congruent with existing design work.</li>
+					<li>Designed and developed single page promotional websites.</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-12">
+				<h4>Renoworks</h4>
+				<h5>UI/UX Designer & Frontend Developer, June 2014 – April 2017</h5>
+				<ul>
+					<li>Lead designer for new user interfaces.</li>
+					<li>Converted client-provided designs in to templates and visual components compatible with the
+						Renoworks Enterprise platform.</li>
+					<li>Conceptualized and developed the first proof-of-concept for the Renoworks "Color Picker" phone
+						app. This opened the door for Renoworks in to more native phone application development.</li>
+					<li> Lead the charge on developing fully mobile-responsive templates for the existing application in my
+						spare time with another developer.</li>
+					<li>Was the "Go to guy" in the office for any HTML5 or CSS3 related questions or issues.</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-12">
+				<h4>Western Inspection</h4>
+				<h5>Website & Web Application Developer, February 2013 – May 2014</h5>
+				<ul>
+					<li>Tasked with developing a new, cross-platform capable, browser-based business software.</li>
+					<li>Consult and assist in the eventual replacement of workstation and server infrastructure from a
+						Windows 2000 platform to a Linux based system.</li>
+					<li>Maintained and regularly tested commercial alarm system.</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-12">
+				<h4>open2space</h4>
+				<h5>Contract Web Developer, June 2011 – March 2012</h5>
+				<ul>
+					<li> Worked closely with a team developing layouts, views, and controllers in Zend PHP for an enterprise
+						level client.</li>
+					<li>Gained a solid understanding of MVC principles.</li>
+					<li>Developed and maintained a number of small Drupal 6 & 7 websites for new and existing clients.</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-12">
+				<h4>StyleLabs Marketing Group</h4>
+				<h5>Contract Production Manager April 2010 – October 2010</h5>
+				<ul>
+					<li> Liason between marketing and development team. Translated marketing objectives in to
+						development tasks.</li>
+					<li>Played a key role in the conceptualization and initial development of a proprietary CMS.</li>
+					<li>Managed workflow for a small team consisting of myself, a junior developer, and a graphic designer.</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-12">
+				<h4>Cambrian House</h4>
+				<h5>Junior Web Developer, September 2006 – March 2007</h5>
+				<ul>
+					<li>Worked on a variety of different small start-up website projects launched from Cambrian House's
+						crowdsourcing platform.</li>
+					<li>Converted PSD's from the design team in to HTML and CSS websites.</li>
+					<li>Worked to ensure cross-browser compatibility between major browsers.</li>
+					<li>Discovered and helped patch a security vulnerability on the company's public forum software</li>
+				</ul>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-12">
+				<h3>Education</h3>
+				<div class="divider"></div>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-12">
+				<h4>Interaction Design Foundation</h4>
+				<h5>Web Design for Usability Course (online)
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-xs-12">
+				<h4>Foothills Composite High School 06'</h4>
+				<h5>High School Diploma</h5>
+				<ul>
+					<li>InfoPro I,II,III; Computer Programming II, III</li>
+					<li>2005 Skills Alberta – Bronze – Website Design</li>
+				</ul>
+			</div>
+		</div>
+
+	</div>
+
+</div>
